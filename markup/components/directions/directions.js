228885a90566e4bf883ef61510cf5d4e1d97c81b
Anton Belyaev <belyash23@inbox.ru>
2021-03-08 19:03:01 +0400

    add directions page

---
+++ markup/components/directions/directions.js
@@ -0,0 +1,30 @@
+$.ajax('http://tor-obr.ru/api/direction', {
+
+}).done((response) => {
+    const directions = response.data;
+    directions.forEach(item => {
+        const direction = $(`<div class="directions-sidebar__direction-card direction-card" data-id="${item.id}">
+                                <div class="direction-card__title">${item.name}</div>
+                                <img class="direction-card__icon" src="${item.icon}" alt="">
+                            </div>`);
+        $('.directions-sidebar__title').after(direction);
+    });
+
+    $('.direction-card').click(e => {
+        $('.direction-info').children().not('.direction-info__to-resources').remove();
+        const id = $(e.target).closest('.direction-card').data('id');
+        $('.direction-info__to-resources').removeClass('disabled').attr('href', `/resources?id=${id}`);
+
+        const direction = directions.find(direction => direction.id === id);
+        const description = $(`<div class="direction-info__description">${direction.description}</div>`);
+        const images = $(`<div class="direction-info__images"></div>`);
+
+        direction.images.forEach(image => {
+            images.append($(`<img class="direction-info__image" src="${image.src}">`));
+        });
+
+        $('.direction-info').prepend(description);
+        $('.direction-info').prepend(images);
+    });
+});
+
